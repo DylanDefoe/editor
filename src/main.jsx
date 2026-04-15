d889c97b855b83d2ec8e0dfc7d9f444ac4021338
jiangxinpeng <jiangxinpeng@xyd.cn>
2026-04-15 16:10:26 +0800

    refactor: 移动变量初始化逻辑至 index.js，删除冗余的 variableInit.js 文件

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { render } from "react-dom";
 import "antd/dist/antd.css";
 import "@wangeditor/editor/dist/css/style.css";
 import "./index.css";
-import "./modules/variableInit";
+import "./modules";
 import App from "./App.jsx";
 import zhCN from 'antd/es/locale/zh_CN';
 import { ConfigProvider } from "antd";
